93efa6c403463dc8b3eeb89f04d1f4603a050f98
PankajJaisu <68227785+PankajJaisu@users.noreply.github.com>
2021-01-26 13:48:53 +0530

    Add files via upload

---
+++ Day2AssignmentQ1.py
@@ -0,0 +1,6 @@
+li=[12,3,55,6,144]
+evenlist=[]
+for i in li:
+    if i%2==0:
+        evenlist.append(i)
+print(evenlist)
---
+++ Day2AssignmentQ2.py
@@ -0,0 +1,5 @@
+n=6
+for i in range(1,n):
+    q=n
+    print(" "*q,str(i)*i)
+    n=n-1
